7313b36193bd5c7923a816c6887e7c9354e7d64f
kaneadam <kane.adam@gmail.com>
2026-09-24 13:22:37 -0700

    Create JobTrackerApp class for application setup

---
+++ app/src/main/java/com/example/jobtracker/JobTrackerApp.kt
@@ -0,0 +1,10 @@
+package com.example.jobtracker
+
+import android.app.Application
+
+class JobTrackerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Will be expanded later for notification channels and WorkManager
+    }
+}
